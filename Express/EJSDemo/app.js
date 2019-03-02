59fc0e008054b641c360f00c5ffe1477493fcc2d
vinodpatidar123 <pvinod813@gmail.com>
2019-03-02 14:25:49 +0530

    minor update

--- Express/EJSDemo/app.js
+++ Express/EJSDemo/app.js
@@ -1,8 +1,11 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 app.set("view engine","ejs");
+
 // Root Route (home)
 app.get("/",function(req,res){
     res.render("home");
@@ -13,12 +16,19 @@ app.get("/avengers",function(req,res){
     res.render("avengers");
 });
 
+app.post("/addavenger",function(req,res){
+    var newAvenger = req.body.newavenger;
+    res.redirect("avengers");
+});
+
 // Passing parameter in render()
 app.get("/avenger/:names",function(req,res){
     var name = req.params.names;
     res.render("avenger",{avname:name});
     // res.send("Avenger Name : "+name);
 });
+
+
 
 
 
