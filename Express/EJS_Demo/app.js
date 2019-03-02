2844493399a43724e04b40e39da16f9f547bf0cd
vinodpatidar123 <pvinod813@gmail.com>
2019-03-02 11:53:26 +0530

    EJS Demo with render function

---
+++ Express/EJS_Demo/app.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var app = express();
+
+// Root Route (home)
+app.get("/",function(req,res){
+    res.render("home.ejs");
+    // res.send("<h1>Welcome to Home Page</h1>");
+});
+
+app.get("/avenger",function(req,res){
+    res.render("avengers.ejs");
+});
+
+// Passing parameter in render()
+app.get("/avenger/:names",function(req,res){
+    var name = req.params.names;
+    res.render("avenger.ejs",{avname:name});
+    // res.send("Avenger Name : "+name);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Listening at port 3000
+app.listen(3000,function(){
+    console.log("Server starting at port 3000 ....");
+});
